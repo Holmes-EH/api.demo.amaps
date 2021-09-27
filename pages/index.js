23dd41f0e615bef8de22824e9369fc73a7a26bca
Holmes-EH <holmes.samuel@yahoo.fr>
2021-09-27 21:23:25 +0200

    style the silliness of homepage a little

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,16 @@ import Head from 'next/head'
 
 export default function Home() {
 	return (
-		<div className='container'>
+		<div
+			style={{
+				display: 'flex',
+				height: '100vh',
+				padding: '2em',
+				flexDirection: 'column',
+				justifyContent: 'space-between',
+				alignItems: 'center',
+			}}
+		>
 			<Head>
 				<title>Juju 2 Fruits</title>
 				<meta
@@ -14,6 +23,7 @@ export default function Home() {
 			<div>
 				<h1>When life gives you lemons...</h1>
 			</div>
+			<footer>Make some juice !</footer>
 		</div>
 	)
 }
