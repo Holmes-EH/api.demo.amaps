0fb53dae0c48da3f33d05d658ca2e26dfab130dd
Holmes-EH <holmes.samuel@yahoo.fr>
2021-09-27 19:48:44 +0200

    do some silly stuff on landing page

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,16 @@ export default function Home() {
 	return (
 		<div className='container'>
 			<Head>
-				<title>Admin - Juju 2 Fruits</title>
+				<title>Juju 2 Fruits</title>
 				<meta
 					name='description'
 					content='Back Office pour la gestion des commandes Juju 2 Fruits'
 				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
+			<div>
+				<h1>When life gives you lemons...</h1>
+			</div>
 		</div>
 	)
 }
