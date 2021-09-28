3623bb81469e689196aa8a081f9fe2d5222dd86d
Holmes-EH <holmes.samuel@yahoo.fr>
2021-09-28 22:20:26 +0200

    move accessCode duplicate check to function

--- utils/generateAccessCode.js
+++ utils/generateAccessCode.js
@@ -1,11 +1,28 @@
-import { allowedStatusCodes } from 'next/dist/lib/load-custom-routes'
-const numbers = '0123456789'
+import Amap from '@/models/amapModel'
+import dbConnect from '@/lib/dbConnect.js'
 
-const generateAccessCode = (length) => {
+const numbers = '0123456789'
+dbConnect()
+
+const generateAccessCode = async (length) => {
+	const amapAccessCodesResult = await Amap.find({}).select('accessCode')
+	let amapAccessCodes = []
+	amapAccessCodesResult.map((object) => {
+		amapAccessCodes.push(object.accessCode)
+	})
 	let code = ''
+
 	for (let index = 0; index < length; index++) {
 		code += numbers.charAt(Math.floor(Math.random() * 10))
 	}
+
+	// While loop to make sure generated accessCode is unique in db
+	while (amapAccessCodes.includes(code)) {
+		for (let index = 0; index < length; index++) {
+			code += numbers.charAt(Math.floor(Math.random() * 10))
+		}
+	}
+
 	return code
 }
 
